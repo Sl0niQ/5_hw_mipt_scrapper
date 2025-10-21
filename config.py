22af1277960ba317ca7d26101b0e5502658197bc
Sl0niQ <31eriflleh@gmail.com>
2025-10-21 13:26:18 +0700

    config.py added; iss_data_analyze.py: method for plotting graph added

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+"Конфигурация: простые константы"
+
+FILE_PATH = 'iss_data.csv' # файл с сырыми данными
+CSV_SEPARATOR = ';'        # тип разделителя в csv файле
+SPEED_SUFFIX_LENGTH = 5    # для отсечения 5ти символов ' km/h'
--- iss_data_analyze.py
+++ iss_data_analyze.py
@@ -1,25 +1,14 @@
+# iss_data_analyze.py
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
+from config import FILE_PATH, CSV_SEPARATOR, SPEED_SUFFIX_LENGTH
 
-FILE_PATH = 'iss_data.csv' # имя файла с сырыми данными
-CSV_SEPARATOR = ';'        # разделитель в csv файле с данными
-SPEED_SUFFIX_LENGTH = 5    # для отсечения 5ти символов ' km/h'
-
-if os.path.isfile(FILE_PATH):
-
-	# чтение csv файла
-	df = pd.read_csv(FILE_PATH, usecols=['date', 'speed'], sep=CSV_SEPARATOR)
-
-	# обработка данных, преобразование типов
-	df['speed'] = df['speed'].str[:-SPEED_SUFFIX_LENGTH].astype(float)
-	df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
-
-	# анализ данных
-	max_speed = df['speed'].max()
-	min_speed = df['speed'].min()
-
+def plot_a_graph(df: pd.DataFrame, max_speed: float, min_speed: float) -> None:
+	"""
+	Строит график зависимости скорости МКС от текущего времени
+	"""
 	# построение графика
 	plt.figure(figsize=(12, 6))
 	plt.plot(df['date'], df['speed'], linewidth=2)
@@ -35,5 +24,20 @@ if os.path.isfile(FILE_PATH):
 	# вывод на экран
 	plt.show()
 
+if os.path.isfile(FILE_PATH):
+
+	# чтение csv файла
+	df = pd.read_csv(FILE_PATH, usecols=['date', 'speed'], sep=CSV_SEPARATOR)
+
+	# обработка данных, преобразование типов
+	df['speed'] = df['speed'].str[:-SPEED_SUFFIX_LENGTH].astype(float)
+	df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d %H:%M:%S')
+
+	# анализ данных
+	max_speed = df['speed'].max()
+	min_speed = df['speed'].min()
+
+	plot_a_graph(df, max_speed, min_speed)
+	
 else:
 	print(f"файл {file_path} не найден")
